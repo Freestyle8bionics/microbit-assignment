427f9ce959b54a7f26b1472c7c585651aa12cff4
Freestyle bionics <135496603+Freestyle8bionics@users.noreply.github.com>
2024-06-25 10:24:56 +1000

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-huskylens.init_i2c()
-huskylens.init_mode(protocolAlgorithm.ALGORITHM_FACE_RECOGNITION)
-
-def on_forever():
-    huskylens.request()
-    serial.write_value("box", huskylens.readBox_ss(1, Content3.ID))
-basic.forever(on_forever)
