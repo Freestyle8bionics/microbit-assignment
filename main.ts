c4e3d37aa717f1c20186511f7d7aae554e0c477b
Freestyle bionics <135496603+Freestyle8bionics@users.noreply.github.com>
2024-06-25 10:24:38 +1000

    Delete main.ts

--- main.ts
+++
@@ -1,6 +0,0 @@
-huskylens.initI2c()
-huskylens.initMode(protocolAlgorithm.ALGORITHM_FACE_RECOGNITION)
-basic.forever(function on_forever() {
-    huskylens.request()
-    serial.writeValue("box", huskylens.readBox_ss(1, Content3.ID))
-})
